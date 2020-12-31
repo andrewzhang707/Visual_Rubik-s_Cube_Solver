9d85441f6c7a07e5bf5d554639900f9074141d2b
Andrew Zhang <andrewzhang707@gmail.com>
2020-12-31 17:59:16 -0500

    Finished turn functions

--- CubeSolver.py
+++ CubeSolver.py
@@ -6,9 +6,9 @@ import enum
 class Colors(enum.Enum):
     Yellow = 0
     Blue = 1
-    Red = 2
+    Orange = 2
     Green = 3
-    Orange = 4
+    Red = 4
     White = 5
 
 GREEN = (0, 255, 0)
@@ -159,20 +159,27 @@ class Face:
         self.top = None
         self.bottom = None
     def turn_right():
-        top_orientations = [0,0,0,2,2,2,0,1,2,2,1,0]
-        row = self.color[1][1] * 3 
-        temp[0] = self.bottom.colors[0][2:]
-        temp[1] = self.bottom.colors[1][2:]
-        temp[2] = self.bottom.colors[2][2:]
-        self.bottom.colors[0][2:] = self.right.right.colors[2][0:1]
-        self.bottom.colors[1][2:] = self.right.right.colors[1][0:1]
-        self.bottom.colors[2][2:] = self.right.right.colors[0][0:1]
-        self.right.right.colors[0][0] = self.top.colors[2][2:]
-        self.right.right.colors[1][0] = self.top.colors[1][2:]
-        self.right.right.colors[2][0] = self.top.colors[0][2:]
-        self.top.colors[0][2]=self.colors[0][2:]
-        self.top.colors[1][2]=self.colors[1][2:]
-        self.top.colors[2][2]=self.colors[2][2:]
+        top_orientations = [2,1,0,2,2,2,0,1,2,0,0,0]
+        bottom_orientations = [0,1,2,2,2,2,2,1,0,0,0,0]
+        bottom_color = self.color[1][1]
+        if(bottom_color%2 == 0):
+            bottom_color = bottom_color + (2*pow((-1),(bottom_color/2+1)))
+        row_start = 3 * (bottom_color - 1)
+        col_start = (3*bottom_color) % 12
+        temp[0] = self.bottom.colors[bottom_orientations[row_start]:bottom_orientations[row_start]+1][bottom_orientations[col_start]:bottom_orientations[col_start]+1]
+        temp[1] = self.bottom.colors[bottom_orientations[row_start+1]:bottom_orientations[row_start+1]+1][bottom_orientations[col_start+1]:bottom_orientations[col_start+1]+1]
+        temp[2] = self.bottom.colors[bottom_orientations[row_start+2]:bottom_orientations[row_start+2]+1][bottom_orientations[col_start+2]:bottom_orientations[col_start+2]+1]
+        row_start = 3 * (self.color[1][1] - 1)
+        col_start = (3*self.color[1][1]) % 12
+        self.bottom.colors[bottom_orientations[row_start]:bottom_orientations[row_start]+1][bottom_orientations[col_start]:bottom_orientations[col_start]+1] = self.right.right.colors[2][0:1]
+        self.bottom.colors[bottom_orientations[row_start+1]:bottom_orientations[row_start+1]+1][bottom_orientations[col_start+1]:bottom_orientations[col_start+1]+1] = self.right.right.colors[1][0:1]
+        self.bottom.colors[bottom_orientations[row_start+2]:bottom_orientations[row_start+2]+1][bottom_orientations[col_start+2]:bottom_orientations[col_start+2]+1] = self.right.right.colors[0][0:1]
+        self.right.right.colors[0][0] = self.top.colors[top_orientations[row_start]:top_orientations[row_start]+1]][top_orientations[col_start]:top_orientations[col_start]+1]
+        self.right.right.colors[1][0] = self.top.colors[top_orientations[row_start+1]:top_orientations[row_start+1]+1]][top_orientations[col_start+1]:top_orientations[col_start+1]+1]
+        self.right.right.colors[2][0] = self.top.colors[top_orientations[col_start+2]:top_orientations[col_start+2]+1]][top_orientations[col_start+2]:top_orientations[col_start+2]+1]
+        self.top.colors[top_orientations[row_start]:top_orientations[row_start]+1]][top_orientations[col_start]:top_orientations[col_start]+1] =self.colors[0][2:]
+        self.top.colors[top_orientations[row_start+1]:top_orientations[row_start+1]+1]][top_orientations[col_start+1]:top_orientations[col_start+1]+1] =self.colors[1][2:]
+        self.top.colors[top_orientations[col_start+2]:top_orientations[col_start+2]+1]][top_orientations[col_start+2]:top_orientations[col_start+2]+1] =self.colors[2][2:]
         self.colors[0][2]=temp[0:1]
         self.colors[1][2]=temp[1:2]
         self.colors[2][2]=temp[2:]
@@ -191,32 +198,41 @@ class Face:
         self.right.colors[1][2] = temp[2:]
         self.right.colors[0][0] = stemp  
     def turn_left():
-        temp[0] = self.bottom.colors[0][0:1]
-        temp[1] = self.bottom.colors[1][0:1]
-        temp[2] = self.bottom.colors[2][0:1]
-        self.bottom.colors[0][0:1] = self.left.left.colors[2][2:]
-        self.bottom.colors[1][0:1] = self.left.left.colors[1][2:]
-        self.bottom.colors[2][0:1] = self.left.left.colors[0][2:]
-        self.left.left.colors[0][2] = self.top.colors[2][0:1]
-        self.left.left.colors[1][2] = self.top.colors[1][0:1]
-        self.left.left.colors[2][2] = self.top.colors[0][0:1]
-        self.top.colors[0][0]=self.colors[0][0:1]
-        self.top.colors[1][0]=self.colors[1][0:1]
-        self.top.colors[2][0]=self.colors[2][0:1]
-        self.colors[0][0]=temp[0:1]
-        self.colors[1][0]=temp[1:2]
-        self.colors[2][0]=temp[2:]
-        stemp = self.left.colors[0][0:1]
-        temp[0] = self.left.colors[1][0:1]
-        temp[1] = self.left.colors[2][0:1]
-        temp[2] = self.left.colors[2][1:2]
+        top_orientations = [0,1,2,0,0,0,2,1,0,2,2,2]
+        bottom_orientations = [2,1,0,2,2,2,0,1,2,0,0,0]
+        row_start = 3 * (self.color[1][1] - 1)
+        col_start = (9 + 3 * (self.color[1][1] - 1)) % 12
+        temp[0] = self.bottom.colors[bottom_orientations[row_start]][bottom_orientations[col_start]:bottom_orientations[col_start] + 1]
+        temp[1] = self.bottom.colors[bottom_orientations[row_start + 1]][bottom_orientations[col_start + 1]:bottom_orientations[col_start + 1]+1]
+        temp[2] = self.bottom.colors[bottom_orientations[row_start + 2]][bottom_orientations[col_start + 2]:bottom_orientations[col_start + 2] + 1]
+        self.bottom.colors[bottom_orientations[row_start]][bottom_orientations[col_start]:bottom_orientations[col_start] + 1] = self.colors[0][0:1]
+        self.bottom.colors[bottom_orientations[row_start + 1]][bottom_orientations[col_start + 1]:bottom_orientations[col_start + 1]+1] = self.colors[1][0:1]
+        self.bottom.colors[bottom_orientations[row_start + 2]][bottom_orientations[col_start + 2]:bottom_orientations[col_start + 2] + 1] = self.colors[2][0:1]
+        row_start = 3 * (self.color[1][1] - 1)
+        col_start = (3 * self.color[1][1]) % 12
+        self.colors[0][0:1] = self.top.colors[top_orientations[row_start]:top_orientations[row_start]+1]][top_orientations[col_start]:top_orientations[col_start]+1]
+        self.colors[1][0:1] = self.top.colors[top_orientations[row_start+1]:top_orientations[row_start+1]+1]][top_orientations[col_start+1]:top_orientations[col_start+1]+1]
+        self.colors[2][0:1] = self.top.colors[top_orientations[col_start+2]:top_orientations[col_start+2]+1]][top_orientations[col_start+2]:top_orientations[col_start+2]+1]
+        self.top.colors[top_orientations[row_start]:top_orientations[row_start]+1]][top_orientations[col_start]:top_orientations[col_start]+1] = self.left.left.colors[0][2:]
+        self.top.colors[top_orientations[row_start+1]:top_orientations[row_start+1]+1]][top_orientations[col_start+1]:top_orientations[col_start+1]+1] = self.left.left.colors[1][2:]
+        self.top.colors[top_orientations[col_start+2]:top_orientations[col_start+2]+1]][top_orientations[col_start+2]:top_orientations[col_start+2]+1] = self.left.left.colors[2][2:]
+        self.left.left.colors[0][2:] = temp[0:1]
+        self.left.left.colors[1][2:] = temp[1:2]
+        self.left.left.colors[2][2:] = temp[2:]
+        temp_index = 0
+        add_array = [2,0,-2]
+        stemp = colors[2][0:1]
         for i in range(0,3):
-            for j in range(0,3):
-                self.left.colors[j][i] = self.left.colors[i][2-j:3-j]
-        self.left.colors[2][o] = stemp
-        self.left.colors[2][1] = temp[0:1]
-        self.left.colors[2][2] = temp[1:2]
+            for j in range(1,4):
+                if(j!=1 and i != 2):
+                    temp_index = temp_index if temp_index != 3 else 2
+                    temp[temp_index]=self.left.colors[3-j][i:i+1]
+                    temp_index += 1
+                self.right.colors[3-j][i]=self.left.colors[i+add_array[i]][3-j:4-j] 
+        self.left.colors[0][1] = temp[0:1]
+        self.left.colors[0][2] = temp[1:2]
         self.left.colors[1][2] = temp[2:]
+        self.left.colors[0][0] = stemp
     def turn_upper():
         temp[0] = self.colors[0][0:1]
         temp[1] = self.colors[0][1:2]
